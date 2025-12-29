182458d50ba4ba3c76abe5c2fe1b727b649e48c6
Alexander Grothe <seahawk1986@gmx.de>
2025-12-30 00:37:30 +0100

    update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,29 +5,47 @@
  */
 
 // Composables
-import { createRouter, createWebHistory } from 'vue-router/auto'
-import { setupLayouts } from 'virtual:generated-layouts'
-import { routes } from 'vue-router/auto-routes'
+import {
+  createRouter,
+  createWebHistory,
+  type NavigationFailure,
+  type RouteLocationNormalizedLoaded,
+} from "vue-router";
+import { setupLayouts } from "virtual:generated-layouts";
+import { routes } from "vue-router/auto-routes";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
-})
+});
 
 // Workaround for https://github.com/vitejs/vite/issues/11804
-router.onError((err, to) => {
-  if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-    if (!localStorage.getItem('vuetify:dynamic-reload')) {
-      console.log('Reloading page to fix dynamic import error')
-      localStorage.setItem('vuetify:dynamic-reload', 'true')
-      location.assign(to.fullPath)
+router.onError(
+  (err: NavigationFailure | Error, to?: RouteLocationNormalizedLoaded) => {
+    if (
+      err?.message?.includes?.("Failed to fetch dynamically imported module")
+    ) {
+      if (!localStorage.getItem("vuetify:dynamic-reload")) {
+        console.log("Reloading page to fix dynamic import error");
+        localStorage.setItem("vuetify:dynamic-reload", "true");
+
+        if (to) {
+          location.assign(to.fullPath);
+        } else {
+          // fallback if route info isn’t available
+          location.reload();
+        }
+      } else {
+        console.error(
+          "Dynamic import error, reloading page did not fix it",
+          err
+        );
+      }
     } else {
-      console.error('Dynamic import error, reloading page did not fix it', err)
+      console.error(err);
     }
-  } else {
-    console.error(err)
   }
-})
+);
 
 router.isReady().then(() => {
   localStorage.removeItem('vuetify:dynamic-reload')
