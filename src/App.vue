44e45220d70d7988cc2ec677a4fd552786c724a2
Alexander Grothe <seahawk1986@gmx.de>
2026-01-01 01:28:06 +0100

    Prevent scrolling of the v-main element for desktop resolutions

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,40 @@
 
 <script setup lang="ts">
 
-import { useLocale } from 'vuetify'
+import { useDisplay, useLocale } from 'vuetify'
+import { useUIStore } from './stores/ui'
 const { current } = useLocale()
+
+const { mobile } = useDisplay()
+const ui = useUIStore()
 
 onMounted(() => {
   document.documentElement.setAttribute('lang', current.value) // for some reason this is needed
 })
 
+
 </script>
 
 <style lang="css">
+/* Enable page scrolling ONLY on mobile */
+@media (max-width: 599px) {
   html, body {
+    overflow-y: auto !important;
+    height: auto !important; /* Critical to let the page grow */
+  }
+}
+
+/* Keep internal scrolling for desktop */
+@media (min-width: 600px) {
+  html, body {
+    overflow: hidden !important;
+  }
+}
+/* allow for overriding the scroll lock */
+.lock-scroll {
   overflow: hidden !important;
-  height: 100%;
+  height: 100dvh !important;
+  position: fixed; /* Extra security for iOS mobile browsers */
+  width: 100%;
 }
 </style>
