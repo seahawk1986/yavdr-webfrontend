a0d6a1ecbeec9503cf37dc492df7f9154e2167b2
Alexander Grothe <seahawk1986@gmx.de>
2026-02-24 09:16:39 +0100

    reformatting

--- src/pages/System/Updates.vue
+++ src/pages/System/Updates.vue
@@ -1,9 +1,6 @@
 <template>
-  <v-sheet
-    class="d-flex flex-column"
-  >
-    <v-card>
-      <v-card-title>Update Packages</v-card-title>
+  <v-sheet class="d-flex flex-column">
+    <v-card prepend-icon="mdi-package-up" title="Update packages">
       <v-card-text>
         <v-btn
           variant="elevated"
@@ -35,50 +32,49 @@
         />
       </v-card-text>
     </v-card>
+    <Shutdown />
   </v-sheet>
 </template>
 
 <script setup lang="ts">
-import { useBackendStore } from '@/stores/backend'
-import { useI18n } from 'vue-i18n'
-const { t } = useI18n()
+import { useBackendStore } from "@/stores/backend";
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
 
-const backend = useBackendStore()
+const backend = useBackendStore();
 
-const allLoadingState: Ref<boolean> = ref(false)
-const debianLoadingState: Ref<boolean> = ref(false)
-const snapLoadingState: Ref<boolean> = ref(false)
-const flatpakLoadingState: Ref<boolean> = ref(false)
+const allLoadingState: Ref<boolean> = ref(false);
+const debianLoadingState: Ref<boolean> = ref(false);
+const snapLoadingState: Ref<boolean> = ref(false);
+const flatpakLoadingState: Ref<boolean> = ref(false);
 
 // const sleep = async (ms: number): Promise<void> => {
 //   return new Promise((resolve) => setTimeout(resolve, ms));
 // }
 
 async function updateAll() {
-  await update("all", allLoadingState)
+  await update("all", allLoadingState);
 }
 
 async function updateDebian() {
-  await update("debian", debianLoadingState)
+  await update("debian", debianLoadingState);
 }
 async function updateSnaps() {
-  await update("snap", snapLoadingState)
+  await update("snap", snapLoadingState);
 }
 async function updateFlatpak() {
-  await update("flatpak", flatpakLoadingState)
+  await update("flatpak", flatpakLoadingState);
 }
 
 async function update(what: string, stateHandler: Ref<boolean>) {
-  stateHandler.value = true
+  stateHandler.value = true;
   try {
-    const success = await backend.postRequest(`/system/update/${what}`, {})
-    console.log("update result:", success.data)
-
-  } catch(error) {
-    console.log(`Updating ${what} failed: ${error}`)
+    const success = await backend.postRequest(`/system/update/${what}`, {});
+    console.log("update result:", success.data);
+  } catch (error) {
+    console.log(`Updating ${what} failed: ${error}`);
   } finally {
-    stateHandler.value = false
+    stateHandler.value = false;
   }
-
 }
 </script>
